840ed41e213eb188e8d6569739df6ffef1d244dc
Felix Sun <sunfmin@gmail.com>
2018-09-16 14:59:20 +0800

    Fix tricky quoted content problem with k8s_apply

---
+++ example/k8s/tests/test_tricky_configmap.jsonnet
@@ -0,0 +1,16 @@
+// For section a
+local k8s = import 'k8s.jsonnet';
+
+k8s.configmap(
+  namespace='default',
+  name='cm-tricky',
+  deployment='app1',
+  withoutVersion=true,
+  data={
+    Hello: |||
+        Value122'abc'
+        &amp;"hello"
+    |||,
+    MoreTricky: "'Value122'abc&amp;\"hello\"'",
+  }
+)
---
+++ test_with_kubectl.sh
@@ -0,0 +1,4 @@
+./plantbuild k8s_apply ./example/k8s/tests/test_tricky_configmap.jsonnet
+actual=$(kubectl get cm cm-tricky -o json | jq -r '.data.MoreTricky')
+expected=$(./plantbuild show ./example/k8s/tests/test_tricky_configmap.jsonnet | jq -r '.data.MoreTricky')
+diff -B <(printf $actual) <(printf $expected) || (echo "FAILED" && exit 1)
