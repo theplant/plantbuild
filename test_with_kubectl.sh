0575b685aa213e4ca68c3268a2f558ac74b89bb2
Yang Li <idealhack@gmail.com>
2020-07-03 17:28:30 +0800

    added a dry run flag for `k8s_apply`

--- test_with_kubectl.sh
+++ test_with_kubectl.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+./plantbuild k8s_apply ./example/k8s/tests/test_tricky_configmap.jsonnet -d client
+
+./plantbuild k8s_apply ./example/k8s/tests/test_tricky_configmap.jsonnet -d server
+
+./plantbuild k8s_apply ./example/k8s/tests/test_tricky_configmap.jsonnet -d none
+
 ./plantbuild k8s_apply ./example/k8s/tests/test_tricky_configmap.jsonnet
 actual=$(kubectl get cm cm-tricky -o json | jq -r '.data.MoreTricky')
 expected=$(./plantbuild show ./example/k8s/tests/test_tricky_configmap.jsonnet | jq -r '.data.MoreTricky')
